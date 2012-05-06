e4305fc1fb9ec8c38e7a8a42df1d3f434593f763
Petar Vlahu <vlahupetar@gmail.com>
2012-05-06 00:47:50 -0400

    lesson 4 complete

---
+++ lesson4/scheem/scheem.js
@@ -0,0 +1,91 @@
+var evalScheem = function (expr, env) {
+	var l = expr.length;
+    // Numbers evaluate to themselves
+    if (typeof expr === 'number') {
+        return expr;
+    }
+	// Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+	}
+    // Look at head of list for operation
+    switch (expr[0]) {
+        case '+':
+			var sum = 0;
+			for (var i=1; i<l; i++)
+				sum += evalScheem(expr[i], env);
+			if (isNaN(sum))
+				throw new Error("type error: addition is defined for numbers only");
+            return sum;
+        case '-':
+			var sub = evalScheem(expr[1], env);
+			for (var i=2; i<l; i++)
+				sub -= evalScheem(expr[i], env);
+			if (isNaN(sub))
+				throw new Error("type error: subtraction is defined for numbers only");
+            return sub;
+        case '*':
+			var prod = 1;
+			for (var i=1; i<l; i++)
+				prod *= evalScheem(expr[i], env);
+			if (isNaN(prod))
+				throw new Error("type error: multiplication is defined for numbers only");
+            return prod;
+        case '/':
+			var div = evalScheem(expr[1], env);
+			for (var i=2; i<l; i++)
+				div /=evalScheem(expr[i], env);
+			if (isNaN(div))
+				throw new Error("type error: division is defined for numbers only");
+            return div;
+        case 'quote':
+			if (l>2)
+				throw new Error("argument error: quote works with single argument");
+            return expr[1];
+		case 'set!':
+			if (env[expr[1]]===undefined)
+				throw new Error("error: undefined variable "+expr[1]+"; call define first");
+            env[expr[1]]=evalScheem(expr[2], env);
+            return env;
+        case 'define':
+            env[expr[1]]=evalScheem(expr[2], env);
+            return 0;
+		case 'begin':
+            var res;
+            for (var i=1;i<expr.length;i++)
+                res=evalScheem(expr[i], env);
+            return res;
+		case '<':
+            var eq = evalScheem(expr[1],env)<evalScheem(expr[2],env);
+            return eq?'#t':'#f';
+		case '>':
+            var eq = evalScheem(expr[1],env)>evalScheem(expr[2],env);
+            return eq?'#t':'#f';
+		case 'cons':
+            return [evalScheem(expr[1],env)]
+                .concat(evalScheem(expr[2],env));
+        case 'car':
+            return evalScheem(expr[1], env).shift();
+        case 'cdr':
+            return evalScheem(expr[1], env).slice(1);
+		case '=':
+            var eq =
+                (evalScheem(expr[1], env) ===
+                 evalScheem(expr[2], env));
+            if (eq) return '#t';
+            return '#f';
+        case 'if':
+			if (l!=4)
+				throw new Error('argument error: if expression accepts 3 arguments');
+			var predicate = evalScheem(expr[1], env);
+			if (predicate === '#t')
+				return evalScheem(expr[2], env);
+			if (predicate === '#f')
+				return evalScheem(expr[3], env);
+			throw new Error('argument error: predicate doesn\'t evaluate to bool \n' + expr[1]);
+    }
+};
+
+var evalScheemString = function(sexpr, env){
+	return evalScheem(SCHEEM.parse(sexpr), env);
+}
---
+++ lesson4/tests/tests.js
@@ -0,0 +1,323 @@
+suite('quote', function() {
+    test('a number', function() {
+        assert.deepEqual(
+            evalScheem(['quote', 3], {}),
+            3
+        );
+    });
+    test('an atom', function() {
+        assert.deepEqual(
+            evalScheem(['quote', 'dog'], {}),
+            'dog'
+        );
+    });
+    test('a list', function() {
+        assert.deepEqual(
+            evalScheem(['quote', [1, 2, 3]], {}),
+            [1, 2, 3]
+        );
+    });
+	test('argument error', function() {
+		assert.throws(function(){
+			evalScheem(['quote', [1, 2, 3], []], {});
+		});
+	});
+});
+suite('add', function() {
+    test('two numbers', function() {
+        assert.deepEqual(
+            evalScheem(['+', 3, 5], {}),
+            8
+        );
+    });
+	test('multiple nums', function() {
+		assert.deepEqual(
+			evalScheem(['+',2,2,3,4,5,6.7,3.5], {}),
+			2+2+3+4+5+6.7+3.5
+		);
+	});
+    test('a number and an expression', function() {
+        assert.deepEqual(
+            evalScheem(['+', 3, ['+', 2, 2]], {}),
+            7
+        );
+    });
+	test('a dog and a cat', function() {
+		assert.throws(function(){
+			evalScheem(['+', 'dog', 'cat'], {})
+		});
+	});
+});
+
+suite('subtract', function() {
+    test('two numbers', function() {
+        assert.deepEqual(
+            evalScheem(['-', 3, 5], {}),
+            -2
+        );
+    });
+	test('multiple nums', function() {
+		assert.deepEqual(
+			evalScheem(['-', 2,2,3,45], {}),
+			2-2-3-45
+		);
+	});
+    test('a number and an expression', function() {
+        assert.deepEqual(
+            evalScheem(['-', 3, ['-', 5, 2]], {}),
+            0
+        );
+    });
+	test('type error', function(){
+		assert.throws(function(){
+			evalScheem(['-', 3,4,5,'e','b'])
+		});
+	});
+});
+
+suite('multiply', function() {
+    test('two numbers', function() {
+        assert.deepEqual(
+            evalScheem(['*', 3, 5], {}),
+            15
+        );
+    });
+    test('a number and an expression', function() {
+        assert.deepEqual(
+            evalScheem(['*', 3, ['*', 5, 2]], {}),
+            30
+        );
+    });
+	test('multi numbers', function() {
+        assert.deepEqual(
+            evalScheem(['*', 3, 5, 6, 7, 8, 9], {}),
+            3* 5* 6* 7* 8* 9
+        );
+    });
+	test('type error', function(){
+		assert.throws(function(){
+			evalScheem(['*', 3,4,5,'e','b'])
+		});
+	});
+});
+
+suite('divide', function() {
+    test('two numbers', function() {
+        assert.deepEqual(
+            evalScheem(['/', 3, 5], {}),
+            3/5
+        );
+    });
+    test('a number and an expression', function() {
+        assert.deepEqual(
+            evalScheem(['/', 3, ['/', 5, 2]], {}),
+            3/(5/2)
+        );
+    });
+	test('multi numbers', function() {
+        assert.deepEqual(
+            evalScheem(['/', 3, 5, 6, 7, 8, 9], {}),
+            3/ 5/ 6/ 7/ 8/ 9
+        );
+    });
+	test('type error', function(){
+		assert.throws(function(){
+			evalScheem(['/', 3,4,5,'e','b'])
+		});
+	});
+});
+
+suite('var evaluate', function(){
+	
+	var env = {x:2, y:3, z:10};
+	test('5 test',function(){
+		assert.deepEqual(
+			evalScheem(5, env), 
+			5
+		);
+	});
+	test('x test',function(){
+		assert.deepEqual(
+			evalScheem('x', env), 
+			2
+		);
+	});
+	test('(+ 2 3) test',function(){
+		assert.deepEqual(
+			evalScheem(['+', 2, 3], env), 
+			5
+		);
+	});
+	test('(* y 3) test',function(){
+		assert.deepEqual(
+			evalScheem(['*', 'y', 3], env), 
+			9
+		);
+	});
+	test('(/ z (+ x y)) test',function(){
+		assert.deepEqual(
+			evalScheem(['/', 'z', ['+', 'x', 'y']], env), 
+			2
+		);
+	});
+	
+});
+suite('define/set!', function(){
+	var env = {x:2, y:3, z:10};
+	test( 'x test',function(){
+		assert.deepEqual(evalScheem('x', env), 2)
+	});
+	
+	test( 'evaluation of define test',function(){
+		var tmp = evalScheem(['define', 'a', 5], env);
+		assert.deepEqual(tmp, 0)
+	});
+	test( '(define a 5) test',function(){
+		assert.deepEqual(env, {x:2, y:3, z:10, a:5})
+	});
+	test( '(set! a 1) test',function(){
+		var tmp = evalScheem(['set!', 'a', 1], env);
+		assert.deepEqual(env, {x:2, y:3, z:10, a:1})
+	});
+	
+	test( '(set! x 7) test',function(){
+		var tmp = evalScheem(['set!', 'x', 7], env);
+		assert.deepEqual(env, {x:7, y:3, z:10, a:1})
+	});
+	test( '(set! y (+ x 1)) test',function(){
+		var tmp = evalScheem(['set!', 'y', ['+', 'x', 1]], env);
+		assert.deepEqual(env, {x:7, y:8, z:10, a:1})
+	});
+	test('type error', function(){
+		assert.throws(function(){
+			evalScheem(['set!', 'y', ['+', 'x', 1]], {y:0})
+		});
+	});
+});
+
+suite('begin expression', function(){
+	test( '(begin 1 2 3) test',function(){
+		assert.deepEqual(evalScheem(['begin', 1, 2, 3], {}), 3);
+	});
+	test( '(begin (+ 2 2)) test',function(){
+		assert.deepEqual(evalScheem(['begin', ['+', 2, 2]], {}), 4);
+	});
+	test( '(begin x y x) test',function(){
+		assert.deepEqual(evalScheem(['begin', 'x', 'y', 'x'], {x:1, y:2}), 1);
+	});
+	test( '(begin (set! x 5) (set! x (+ y x) x)) test',function(){
+		assert.deepEqual(
+			evalScheem(
+				['begin', 
+					['set!', 'x', 5], 
+					['set!', 'x', ['+', 'y', 'x']], 
+				'x'], 
+				{x:1, y:2}
+			), 7);
+	});
+});
+
+suite('quotes', function(){
+	test( '(+ 2 3) test',function(){
+		assert.deepEqual(evalScheem(['+', 2, 3], {}), 5);
+	});
+	test( '(quote (+ 2 3)) test',function(){
+		assert.deepEqual(evalScheem(['quote', ['+', 2, 3]], {}), ['+', 2, 3]);
+	});
+	test( '(quote (quote (+ 2 3))) test',function(){
+		assert.deepEqual(evalScheem(
+			['quote', 
+				['quote', ['+', 2, 3]]
+			], {}), 
+			['quote', ['+', 2, 3]]
+		);
+	});
+});
+
+suite('number ops', function(){
+	test( '(+ 2 3) test',function(){
+		assert.deepEqual(evalScheem(['+', 2, 3], {}), 5);
+	});
+	test( '(< 2 2) test',function(){
+		assert.deepEqual(evalScheem(['<', 2, 2], {}), '#f');
+	});
+	test( '(< 2 3) test',function(){
+		assert.deepEqual(evalScheem(['<', 2, 3], {}), '#t');
+	});
+	test( '(< (+ 1 1) (+ 2 3)) test',function(){
+		assert.deepEqual(evalScheem(['<', ['+', 1, 1], ['+', 2, 3]], {}), '#t');
+	});
+});
+
+suite('list ops', function(){
+	test('(quote (2 3)) test',function(){
+		assert.deepEqual(evalScheem(['quote', [2, 3]], {}), [2, 3]);
+	});
+	test("(cons 1 '(2 3)) test",function(){
+		assert.deepEqual(evalScheem(['cons', 1, ['quote', [2, 3]]], {}),[1, 2, 3]);
+	});
+	test("(cons '(1 2) '(3 4)) test",function(){
+		assert.deepEqual(evalScheem(['cons', ['quote', [1, 2]], ['quote',[3, 4]]], {}), [[1, 2], 3, 4]);
+	});
+	test("(car '((1 2) 3 4)) test",function(){
+		assert.deepEqual(evalScheem(['car', ['quote', [[1, 2], 3, 4]]], {}),[1, 2]);
+	});
+	test("(cdr '((1 2) 3 4)) test",function(){
+		assert.deepEqual(evalScheem(['cdr', ['quote', [[1, 2], 3, 4]]], {}),[3, 4]);
+	});
+});
+
+suite('conditionals', function(){
+	test( '(if (= 1 1) 2 3) test',function(){
+		assert.deepEqual(evalScheem(['if', ['=', 1, 1], 2, 3], {}), 2);
+	});
+	test( '(if (= 1 0) 2 3) test',function(){
+		assert.deepEqual(evalScheem(['if', ['=', 1, 0], 2, 3], {}), 3);
+	});
+	test( '(if (= 1 1) 2 error) test',function(){
+		assert.deepEqual(evalScheem(['if', ['=', 1, 1], 2, 'error'], {}), 2);
+	});
+	test( '(if (= 1 1) error 3) test',function(){
+		assert.deepEqual(evalScheem(['if', ['=', 1, 0], 'error', 3], {}), 3);
+	});
+	test( '(if (= 1 1) (if (= 2 3) 10 11) 12) test',function(){
+		assert.deepEqual(evalScheem(['if', ['=', 1, 1], ['if', ['=', 2, 3], 10, 11], 12], {}), 11);
+	});
+	test('type error', function(){
+		assert.throws(function(){
+			evalScheem(['if', ['+', 1, 0], 2, 3], {})
+		});
+	});
+	test('argument error', function(){
+		assert.throws(function(){
+			evalScheem(['if', ['=', 1, 0], 2], {})
+		});
+	});
+});
+
+suite('parse', function() {
+    test('a number', function() {
+        assert.deepEqual(
+            SCHEEM.parse('42'),
+            42
+        );
+    });
+    test('a variable', function() {
+        assert.deepEqual(
+            SCHEEM.parse('x'),
+            'x'
+        );
+    });
+	test('complex scheem parse', function() {
+		assert.deepEqual(
+            evalScheem(SCHEEM.parse("(if (= 1 (+ 1 (- 2 3))) (car '((1 2) (3 4))) '(2 1))"),{}),
+            [2,1]
+        );
+    });
+	test('scheem string parse', function() {
+		assert.deepEqual(
+            evalScheemString("(if (= 1 (+ 1 (- 2 3))) (car '((1 2) (3 4))) '(2 1))",{}),
+            [2,1]
+        );
+    });
+});
